9a51c4c8c6d7ffdac68b56e405ecc3aace0fb4d5
yashikakoppulakonda <yashikacapcut@gmail.com>
2025-12-16 21:06:34 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+    function showPage(pageId) {
+      const pages = document.querySelectorAll('.page');
+      pages.forEach(page => page.classList.remove('active'));
+      document.getElementById(pageId).classList.add('active');
+    }
